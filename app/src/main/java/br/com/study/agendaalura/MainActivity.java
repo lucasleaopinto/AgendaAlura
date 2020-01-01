916a98b7d9ceacbefd26ba4b197c01f5525bab78
lucasleaopinto <lucasleaop@gmail.com>
2020-01-01 11:54:02 -0300

    Add icon no floating button e AppCompatActivity

--- app/src/main/java/br/com/study/agendaalura/MainActivity.java
+++ app/src/main/java/br/com/study/agendaalura/MainActivity.java
@@ -6,17 +6,19 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
+        setTitle("Lista Alunos");
         List<String> alunos = new ArrayList<>(Arrays.asList("Jesus","Maria","Jose","Pedro", "Paulo"));
         ListView listaAluno = findViewById(R.id.activity_main_listView);
         listaAluno.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,alunos));

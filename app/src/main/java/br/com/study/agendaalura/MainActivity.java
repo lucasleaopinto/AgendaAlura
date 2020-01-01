de83121f9d38539c2e807ba32d552c64831dd6b7
lucasleaopinto <lucasleaop@gmail.com>
2020-01-01 11:48:07 -0300

    Primeiro commit

---
+++ app/src/main/java/br/com/study/agendaalura/MainActivity.java
@@ -0,0 +1,24 @@
+package br.com.study.agendaalura;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+        List<String> alunos = new ArrayList<>(Arrays.asList("Jesus","Maria","Jose","Pedro", "Paulo"));
+        ListView listaAluno = findViewById(R.id.activity_main_listView);
+        listaAluno.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,alunos));
+    }
+}
